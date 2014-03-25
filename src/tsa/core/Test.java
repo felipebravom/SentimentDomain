59f8c65db0598fee8670e6e1fd354f781ff80434
felipebravom <admin@weka1.resnet.scms.waikato.ac.nz>
2014-03-25 19:01:38 +1300

    playing with the WEKA API

--- src/tsa/core/Test.java
+++ src/tsa/core/Test.java
@@ -1,177 +1,105 @@
 package tsa.core;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.FastVector;
+import weka.core.SparseInstance;
+import weka.filters.unsupervised.attribute.Add;
 
 public class Test {
 	
-	
-	public static void examp() throws ParseException{
-		  
-			     FastVector      atts;
-			     FastVector      attsRel;
-			     FastVector      attVals;
-			     FastVector      attValsRel;
-			     Instances       data;
-			     Instances       dataRel;
-			     double[]        vals;
-			     double[]        valsRel;
-			     int             i;
-			 
-			     // 1. set up attributes
-			     atts = new FastVector();
-			     // - numeric
-			     atts.addElement(new Attribute("att1"));
-			     // - nominal
-			     attVals = new FastVector();
-			     for (i = 0; i < 5; i++)
-			       attVals.addElement("val" + (i+1));
-			     atts.addElement(new Attribute("att2", attVals));
-			     // - string
-			     atts.addElement(new Attribute("att3", (FastVector) null));
-			     // - date
-			     atts.addElement(new Attribute("att4", "yyyy-MM-dd"));
-			     // - relational
-			     attsRel = new FastVector();
-			     // -- numeric
-			     attsRel.addElement(new Attribute("att5.1"));
-			     // -- nominal
-			     attValsRel = new FastVector();
-			     for (i = 0; i < 5; i++)
-			       attValsRel.addElement("val5." + (i+1));
-			     attsRel.addElement(new Attribute("att5.2", attValsRel));
-			     dataRel = new Instances("att5", attsRel, 0);
-			     atts.addElement(new Attribute("att5", dataRel, 0));
-			 
-			     // 2. create Instances object
-			     data = new Instances("MyRelation", atts, 0);
-			 
-			     // 3. fill with data
-			     // first instance
-			     vals = new double[data.numAttributes()];
-			     // - numeric
-			     vals[0] = Math.PI;
-			     // - nominal
-			     vals[1] = attVals.indexOf("val3");
-			     // - string
-			     vals[2] = data.attribute(2).addStringValue("This is a string!");
-			     // - date
-			     vals[3] = data.attribute(3).parseDate("2001-11-09");
-			     // - relational
-			     dataRel = new Instances(data.attribute(4).relation(), 0);
-			     // -- first instance
-			     valsRel = new double[2];
-			     valsRel[0] = Math.PI + 1;
-			     valsRel[1] = attValsRel.indexOf("val5.3");
-			     dataRel.add(new Instance(1.0, valsRel));
-			     // -- second instance
-			     valsRel = new double[2];
-			     valsRel[0] = Math.PI + 2;
-			     valsRel[1] = attValsRel.indexOf("val5.2");
-			     dataRel.add(new Instance(1.0, valsRel));
-			     vals[4] = data.attribute(4).addRelation(dataRel);
-			     // add
-			     data.add(new Instance(1.0, vals));
-			 
-			     // second instance
-			     vals = new double[data.numAttributes()];  // important: needs NEW array!
-			     // - numeric
-			     vals[0] = Math.E;
-			     // - nominal
-			     vals[1] = attVals.indexOf("val1");
-			     // - string
-			     vals[2] = data.attribute(2).addStringValue("And another one!");
-			     // - date
-			     vals[3] = data.attribute(3).parseDate("2000-12-01");
-			     // - relational
-			     dataRel = new Instances(data.attribute(4).relation(), 0);
-			     // -- first instance
-			     valsRel = new double[2];
-			     valsRel[0] = Math.E + 1;
-			     valsRel[1] = attValsRel.indexOf("val5.4");
-			     dataRel.add(new Instance(1.0, valsRel));
-			     // -- second instance
-			     valsRel = new double[2];
-			     valsRel[0] = Math.E + 2;
-			     valsRel[1] = attValsRel.indexOf("val5.1");
-			     dataRel.add(new Instance(1.0, valsRel));
-			     vals[4] = data.attribute(4).addRelation(dataRel);
-			     // add
-			     data.add(new Instance(1.0, vals));
-			 
-			     // 4. output data
-			     System.out.println(data);
-			  
-			 
-	}
+
 
 	public static void main(String[] args) {
 		
-		FastVector atts=new FastVector();
-		atts.addElement(new Attribute("att1"));
-		atts.addElement(new Attribute("att2")); 
-		Instances inst=new Instances("My Relation", atts, 0);
+		ArrayList<Attribute> att=new ArrayList<Attribute>(); 
+	
+		att.add(new Attribute("att1"));
+		att.add(new Attribute("att2"));
+		
+	
+		ArrayList<String> att3Values=new ArrayList<String>();
+		for(int i=0;i<5;i++)
+			att3Values.add("val" + (i+1));
+		att.add(new Attribute("att3",att3Values));
+		
+		
+
+		att.add(new Attribute("content", (ArrayList<String>) null));
+		
+		
+		ArrayList<String> label=new ArrayList<String>();
+		label.add("positive");
+		label.add("neutral");
+		label.add("negative");
+		
+		att.add(new Attribute("class",label));	
+		
+		Instances dataset=new Instances("Twitter Sentiment Analysis Dataset", att,0); // The last attribute 
+		
+		
 		
 		
 	     
-		System.out.println(inst.toSummaryString());
-		
-		try {
-			examp();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(dataset.toSummaryString());
 		
 		
-		/*
-		 FastVector      atts;
-	     FastVector      attsRel;
-	     FastVector      attVals;
-	     FastVector      attValsRel;
-	     Instances       data;
-	     Instances       dataRel;
-	     double[]        vals;
-	     double[]        valsRel;
-	     int             i;
-	 
-	     // 1. set up attributes
-	     atts = new FastVector();
-	     // - numeric
-	     atts.addElement(new Attribute("att1"));
-	     // - nominal
-	     attVals = new FastVector();
-	     for (i = 0; i < 5; i++)
-	       attVals.addElement("val" + (i+1));
-	     atts.addElement(new Attribute("att2", attVals));
-	     // - string
-	     atts.addElement(new Attribute("att3", (FastVector) null));
-	     // - date
-	     atts.addElement(new Attribute("att4", "yyyy-MM-dd"));
-	     // - relational
-	     attsRel = new FastVector();
-	     // -- numeric
-	     attsRel.addElement(new Attribute("att5.1"));
-	     // -- nominal
-	     attValsRel = new FastVector();
-	     for (i = 0; i < 5; i++)
-	       attValsRel.addElement("val5." + (i+1));
-	     attsRel.addElement(new Attribute("att5.2", attValsRel));
-	     dataRel = new Instances("att5", attsRel, 0);
-	     atts.addElement(new Attribute("att5", dataRel, 0));
-	 
-	     // 2. create Instances object
-	     data = new Instances("MyRelation", atts, 0);
+		// Add data
+		double[] values = new double[dataset.numAttributes()];
+		values[0] = 2.3;
+		values[1] = Math.PI;
+		
+		values[2] = dataset.attribute("att3").indexOfValue("val3");
+		
+		// to retrieve the position of a certain attribute
+		System.out.println("index" + dataset.attribute("att3").index());
+
+		
+		values[3] = dataset.attribute(3).addStringValue("This is a string");
+
+		values[4] = dataset.attribute(4).indexOfValue("neutral");
+		
+		// values[2] = dataset.attribute(2).indexOfValue("val3"); It is also possible to use the numeric index
+	
+		
+		Instance inst = new DenseInstance(1, values);
+		dataset.add(inst);
+
+
+		
+		
+		double values2[] = new double[dataset.numAttributes()];
+		values2[0] = 98;
+		values2[1] = 122;
+		
+		values2[2] = dataset.attribute("att3").indexOfValue("val1");
+				
+		values2[3] = dataset.attribute(3).addStringValue("second string");
+
+		values2[4] = dataset.attribute(4).indexOfValue("positive");
+		
+		Instance inst2 = new DenseInstance(1, values2);
+		dataset.add(inst2);
+
+		
+		
+		System.out.println(dataset.toString());
 		
 		
 		
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
-		*/
+		// Adding a new attribute to my dataset 
+		
+		//Add add;
+		dataset.insertAttributeAt(new Attribute("NewNumeric"), dataset.numAttributes());
+		
+		
+		System.out.println(dataset.toString());
+		
 		
 
 	}
